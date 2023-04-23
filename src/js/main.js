7f23b1273d8e7e080a107af3a702e20e0f877fea
Uffe <uat@aperitas.com>
2023-04-23 11:49:05 +0200

    w

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import * as main from '../../.build/main.js'
 
-main.toast.toast("hello", true)
-main.modal.modal()
+//import * as main from '../../.build/main.js'
+//main.toast.toast("hello", true)
+//main.modal.modal()
--- src/js/premain.js
+++ src/js/premain.js
@@ -1,11 +1,9 @@
 function create_promise(executor) {
-  return new Promise(executor)
-
-
+  return new Promise(executor);
 }
 
 async function create_and_await_promise(executor) {
-   return await create_promise(executor)
+  return await create_promise(executor);
 }
 
-window.create_and_await_promise = create_and_await_promise
+window.create_and_await_promise = create_and_await_promise;
